208d3076e7405d363b0d9d5e6127027baa136669
Dmitriy Sulzhyts <dsulzhits@gmail.com>
2026-01-11 12:45:45 +0300

    Учебные материалы группа PRO513 - Основы Python json

--- _01_basic_python/lesson_17_decorators/_04_json/work_json.py
+++ _01_basic_python/lesson_17_decorators/_04_json/work_json.py
@@ -18,3 +18,13 @@ print(python_data)
 # with open('example.json', 'w', encoding='utf-8') as fp:
 #     json.dump(data, fp, ensure_ascii=False, indent=4)
 
+data = [
+    {'balance': 10000},
+    {'transaction_type': 'доход', 'amount': 15000, 'category': 'аванс'},
+    {'transaction_type': 'расход', 'amount': 5000, 'category': 'жкх'}
+]
+
+print(data[0]['balance'])
+for d in data[1:]:
+    print(d['transaction_type'], d['amount'], d['category'])
+
